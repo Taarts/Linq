9fe7ddfd3297622ec4fe44951ecc5fa22f6ed262
Tricia Amheiser <tricia@taarts.net>
2022-06-02 14:56:25 -0400

    GStark FollowAlong - Long way to double the scores

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+// using System.Linq;
 
-namespace Linq
+using System.Collections.Generic;
+
+namespace linq
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static int MultiplyBy2(int value)
+        {
+            return value * 2;
+        }
+        static void Main(string[] args)
+        {
+            // Here is our original array
+            var scores = new List<int> { 42, 100, 98, 15 };
+
+            // Here is our handy Double-er
+            // Func<int, int> MultiplyBy2 = value => value * 2;
+
+            // Make a new list to store the results
+            var newScores = new List<int>();
+
+            // Go through each score in the scores list
+            foreach (var score in scores)
+            {
+                // Make a doubling of that score
+                var doubled = MultiplyBy2(score);
+
+                // Add it to our new list
+                newScores.Add(doubled);
+            }
+
+            // Print out the scores comma separated
+            Console.WriteLine(String.Join(',', newScores));
+        }
     }
-  }
 }
